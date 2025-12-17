4831b6343937acecde2dcd5efa858643c984ed91
KxD-Work <antonioroberto1407@gmail.com>
2025-12-17 14:03:14 -0400

    starting d10e1

--- day10/ex1.c
+++ day10/ex1.c
@@ -37,13 +37,18 @@ typedef struct {
     size_t capacity;
 } Cells;
 
+#ifdef _WIN32
+typedef SSIZE_T ssize_t;
+#endif
+
 int main(int argc, char **argv) {
     int result = 0;
 
     const char *input = ProcessInput(argc, argv, __FILE__);
     assert(input != NULL);
 
-    Cells mapRows = {};
+    size_t startIndex = (size_t)-1;
+    Cells map         = {};
 
     Nob_String_Builder sb = {};
     if (!nob_read_entire_file(input, &sb))
@@ -67,26 +72,116 @@ int main(int argc, char **argv) {
             case PIPE_SW:
             case PIPE_SE:
             case PIPE_GND:
+                break;
             case PIPE_POS:
+                assert(startIndex == ((size_t)-1));
+                startIndex = map.count;
                 break;
             default:
                 nob_log(NOB_ERROR, "Invalid Cell Shape: " CELL_FMT, CELL_ARGS(c));
                 nob_return_defer(1);
                 break;
             }
-            nob_da_append(&mapRows, c);
+            nob_da_append(&map, c);
         }
 
         lineCount++;
     }
 
-    for (size_t i = 0; i < mapRows.count; ++i) {
-        nob_log(NOB_INFO, CELL_FMT, CELL_ARGS(mapRows.items[i]));
+    assert(startIndex < map.count);
+
+    // Cell start = map.items[startIndex];
+    Cell start = map.items[0];
+    nob_log(NOB_INFO, "Starting Position: " CELL_FMT, CELL_ARGS(start));
+
+    // for (size_t i = 0; i < map.count; ++i) {
+    //     nob_log(NOB_INFO, CELL_FMT, CELL_ARGS(map.items[i]));
+    // }
+
+    const v2i32 mapSize = {map.items[map.count - 1].pos.x + 1, map.items[map.count - 1].pos.y + 1};
+    assert(((size_t)mapSize.x * (size_t)mapSize.y) == map.count);
+
+    nob_log(NOB_INFO, "Map Size: (%d, %d)", mapSize.x, mapSize.y);
+    nob_log(NOB_INFO, "Number of Map Cells: %zu", map.count);
+
+    {
+        // arr2d[y][x]
+        const Cell(*arr2d)[mapSize.x] = (void *)map.items;
+
+        bool up = false;
+        bool dw = false;
+        bool lf = false;
+        bool rt = false;
+
+        const Cell *c = NULL;
+        if (start.pos.x > 0) { // East - Left
+            c = &arr2d[start.pos.y][start.pos.x - 1];
+            if (c->shape == PIPE_EW || c->shape == PIPE_NE || c->shape == PIPE_SE)
+                lf = true;
+        }
+
+        if (start.pos.x < (mapSize.x - 1)) { // West - Right
+            c = &arr2d[start.pos.y][start.pos.x + 1];
+            if (c->shape == PIPE_EW || c->shape == PIPE_NW || c->shape == PIPE_SW)
+                rt = true;
+        }
+
+        if (start.pos.y > 0) { // South - Down
+            c = &arr2d[start.pos.y + 1][start.pos.x];
+            if (c->shape == PIPE_NS || c->shape == PIPE_SW || c->shape == PIPE_SE)
+                dw = true;
+        }
+
+        if (start.pos.y < (mapSize.y - 1)) { // North - Up
+            c = &arr2d[start.pos.y - 1][start.pos.x];
+            if (c->shape == PIPE_NS || c->shape == PIPE_NE || c->shape == PIPE_NW) {
+                up = true;
+            }
+        }
+
+#define BOOLLOG(b) nob_log(NOB_INFO, "%s = %s", (#b), (b) ? "True" : "False")
+
+        BOOLLOG(up);
+        BOOLLOG(dw);
+        BOOLLOG(lf);
+        BOOLLOG(rt);
     }
+
+#if 0
+    for (size_t i = 0; i < map.count; ++i) {
+        i      = startIndex;
+        Cell c = map.items[i];
+        nob_log(NOB_INFO, "Testing Cell: " CELL_FMT, CELL_ARGS(c));
+        // ssize_t index = i + xoff + (yoff * mapSize.x);
+        ssize_t index;
+        for (i32 xoff = -1; xoff < 2; ++xoff) {
+            if (xoff == 0)
+                continue;
+            index = i + xoff;
+
+            if (index < 0 || index >= (ssize_t)map.count)
+                continue;
+
+            nob_log(NOB_INFO, "%c Adjacent Cell: " CELL_FMT, 'X', CELL_ARGS(map.items[index]));
+        }
+
+        for (i32 yoff = -1; yoff < 2; ++yoff) {
+            if (yoff == 0)
+                continue;
+            index = i + (yoff * mapSize.x);
+
+            if (index < 0 || index >= (ssize_t)map.count)
+                continue;
+
+            nob_log(NOB_INFO, "%c Adjacent Cell: " CELL_FMT, 'Y', CELL_ARGS(map.items[index]));
+        }
+        break;
+    }
+#endif
 
 defer:
     nob_sb_free(sb);
-    nob_da_free(mapRows);
+    nob_da_free(map);
 
     return result;
 }
